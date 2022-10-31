6525a05d387d70ecb77f73597ce37b899c62c063
ZuuZaa <zumbaghirova@gmail.com>
2022-10-31 16:46:27 +0400

    nav links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,92 @@
+import { HashLink as Link } from "react-router-hash-link";
+
 export const Navbar = () => {
-    return (
-        <div className="navbar">
-            navbar
-        </div>
-    )
-}
+  const navLinks = [
+    {
+      name: "home",
+      path: "#home",
+      items: [],
+    },
+    {
+      name: "about us",
+      path: "#about",
+      items: [
+        {
+          name: "team",
+          path: "#team",
+          items: [],
+        },
+        {
+          name: "pricing",
+          path: "#pricing",
+          items: [],
+        },
+        {
+          name: "FAQ",
+          path: "#faq",
+          items: [],
+        },
+        {
+          name: "more Links",
+          path: "#",
+          items: [
+            {
+              name: "link 1",
+              path: "/",
+            },
+            {
+              name: "link 2",
+              path: "/",
+            },
+            {
+              name: "link 3",
+              path: "/",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "portfolio",
+      path: "#portfolio",
+      items: [],
+    },
+    {
+      name: "services",
+      path: "#services",
+      items: [],
+    },
+    {
+      name: "testimonals",
+      path: "#testimonals",
+      items: [],
+    },
+    {
+        name: "blog",
+        path: "#blog",
+        items: [],
+      },
+      {
+        name: "contact",
+        path: "#contact",
+        items: [],
+      },
+  ];
+
+  return (
+    <div className="navbar">
+      <div>
+        <Link to="/" className="navbar__logo">oneder</Link>
+      </div>
+      <ul className="navbar__link-list">
+        {navLinks.map((link, index) => (
+          <li key={index}>
+            <Link to={link.path} className="navbar__link">
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
--- src/sections/Home/HomeSection.jsx
+++ src/sections/Home/HomeSection.jsx
@@ -1,9 +1,11 @@
+import { Navbar } from "components/Navbar";
+
 export const HomeSection = () => {
-    return (
-        <header id="home-section">
-            <div className="home-container">
-                header
-            </div>
-        </header>
-    )
-}
+  return (
+    <header id="home-section">
+      <div className="home-container">
+        <Navbar />
+      </div>
+    </header>
+  );
+};
